d94d5a0d8156eda37f8278bdd3a79552a9bb38d1
Tom Bebbington <tophattedcoder@gmail.com>
2013-10-19 13:08:00 +0100

    Tons more docs

--- Main.hx
+++ Main.hx
@@ -1,5 +1,27 @@
-import flash.Boot;
-import js.html.AbstractWorker;
+import flash.Lib;
+import flash.display.Bitmap;
+import openfl.Assets;
+import openfl.display.DirectRenderer;
+import openfl.display.FPS;
+import openfl.display.OpenGLView;
+import openfl.display.Tilesheet;
+import js.html.Document;
+import js.Browser;
+import neko.vm.Module;
+import neko.Boot;
+import neko.Lib;
+import neko.Random;
+import neko.Web;
+import neko.net.ThreadServer;
+import neko.zip.Compress;
+import neko.zip.Uncompress;
+import neko.zip.Flush;
+import cpp.vm.Debugger;
+import cpp.Lib;
+import cpp.FastIterator;
+import cpp.net.Poll;
+import cpp.rtti.FieldIntegerLookup;
+import cpp.vm.Gc;
 class Main {
 	static function main() {
 		
